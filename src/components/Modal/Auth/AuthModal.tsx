5eab4ebe00822158734e67cacb417ab539961371
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-02-02 01:45:06 -0300

    feature: register module

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,14 +1,18 @@
 import {
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Text,
 } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 function AuthModal() {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -24,10 +28,28 @@ function AuthModal() {
     <Modal isOpen={modalState.open} onClose={handleClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader textAlign="center">
+          {modalState.view === 'login' && 'Login'}
+          {modalState.view === 'signup' && 'Sign Up'}
+          {modalState.view === 'resetPassword' && 'Reset Password'}
+        </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <p>aaa</p>
+        <ModalBody
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          pb={6}
+        >
+          <Flex direction="column" align="center" justify="center" width="70%">
+            {/* OAuthButtons */}
+            <OAuthButtons />
+            <Text color="gray.500" fontWeight={700}>
+              OR
+            </Text>
+            <AuthInputs />
+            {/* <ResetPassword /> */}
+          </Flex>
         </ModalBody>
       </ModalContent>
     </Modal>
